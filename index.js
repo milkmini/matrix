0ee48ae94dd2958a573b40ae104dee13b5b81963
Pavel <123minimilk321@mail.ru>
2020-06-11 20:13:26 +0500

    commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// выводим значение инпута
+const valueInput = (id) => {
+    return document.getElementById(id).value;
+}
+// создание матрицы
+function matrix(widthBlock, lengthBlock) {
+    // Переменные схемы матрицы
+    let result = new Array(lengthBlock).fill().map(() => new Array(widthBlock).fill(''));
+    let counter = 1;
+    let startCol = 0;
+    let endCol = widthBlock - 1;
+    let startRow = 0;
+    let endRow = lengthBlock - 1;
+
+    // цикл выполнения матрицы
+    while ( startCol <= endCol && startRow <= endRow ) {
+        // цикл построения первой горизонтали
+        for (let i = startCol; i <= endCol; i++) {
+            result[startRow][i] = counter;
+            counter++;
+        }
+        // опускаем горинзонталь
+        startRow++;
+        // цикл построение первой ввертикали
+        for (let j = startRow; j <= endRow; j++) {
+            result[j][endCol] = counter;
+            counter++;
+        }
+        // сдвигаем вертикаль
+        endCol--;
+        // цикл построения конечной горизонтали
+        for (let i = endCol; i >= startCol; i--) {
+            result[endRow][i] = counter;
+            counter++;
+        }
+        // поднимаем вертикаль
+        endRow--;
+        // цикл построение конечной вертикали
+        for (let j = endRow; j >= startRow; j--) {
+            result[j][startCol] = counter;
+            counter++;
+        }
+        // сдвигаем горизонталь
+        startCol++;
+    }
+    // возвращаем массив матрицы
+    return result;
+}
+// Вырисовка матрицы
+function matrixView( result ) {
+    let rows = '';
+    // делаем построение матницы
+    for(let i = 0; i < result.length; i++) {
+      let cells = '';
+        // построение колоннок
+      for(let j = 0; j < result[i].length; j++) {
+        cells += '<div class="block__col">' + result[i][j] + '</div>';
+      }
+    //   построение горизонталей
+      rows += '<div class="block__row">' + cells + '</div>';
+    }
+    // рисуем их в HTML
+    document.querySelector('body').insertAdjacentHTML('beforeend', '<div class="block">' + rows + '</div>');
+}
+// Построенние матрицы
+document.getElementById('button').onclick = function () {
+    // берем значение из инпутов
+    const widthBlock = Number(valueInput('width'));
+    const lengthBlock = Number(valueInput('length'));
+    // передаем их в функции
+    matrixView(matrix(widthBlock, lengthBlock));
+}
